aa932e875d30268ae1996df40141e4f3ed935af6
RADM90 <joel.jwpark@gmail.com>
2023-02-09 12:35:28 +0900

    amend: missed lines

--- snippets/Built_in/json_.py
+++ snippets/Built_in/json_.py
@@ -1,1 +1,20 @@
 import json
+
+dic = {'4': 5, '6': 7}
+
+"""Encoding"""
+json_str = json.dumps(dic, ensure_ascii=False, separators=(',', ':'), sort_keys=True, indent=None)
+# dumps: dict()를 JSON 문자열로 변환 '{"4": 5, "6": 7}'
+
+with open("test.json", "w+", encoding='utf-8') as f:
+    json.dump(dic, f)
+# dump: JSON 형식의 IO로 변환된 dict()를 파일로 저장
+
+
+"""Decoding"""
+with open("test.json", "r+", encoding='utf-8') as f:
+    data = json.load(f)
+# load: JSON 형식의 파일을 읽어와서 json 객체로 변환
+
+print(json.loads(json_str))
+# loads: JSON 문자열을 읽어와서 json 객체로 변환
--- snippets/DBMS_Conn/mysql_connector.py
+++ snippets/DBMS_Conn/mysql_connector.py
@@ -3,6 +3,59 @@ import pymysql
 USER = 'USER_ID'
 PASSWORD = 'USER_PASSWORD'
 HOST_NAME = 'DBMS_URL'
-PORT = 3306  #
+PORT = 3306  # Default Port
 DB = 'DB_'
 CHARSET = 'utf8'
+
+
+def connect_database():
+    try:
+        Connection = pymysql.connect(user=USER, password=PASSWORD, host=HOST_NAME, port=PORT, database=DB,
+                                     charset=CHARSET)
+        Connection.autocommit = False
+        return Connection
+    except pymysql.Error as e:
+        print(f"Error Connecting to Database: {e}")
+        Connection = pymysql.connect(user=USER, password=PASSWORD, host=HOST_NAME, port=PORT, charset=CHARSET)
+        create_database(Connection, DB, CHARSET)
+        # cursor = Connection.c
+        # create_tables()
+        return Connection
+
+
+def create_database(Connection, db_name, char_set):
+    sql = f"CREATE SCHEMA IF NOT EXISTS `{db_name}` DEFAULT CHARACTER SET {char_set} ;"
+    cursor = Connection.cursor()
+    cursor.execute(sql)
+    Connection.commit()
+
+
+def create_tables(Connection, cursor, db_name, table_name):
+    sql = f"""CREATE TABLE IF NOT EXISTS `{db_name}`.`{table_name}` (
+                              `IMG_INFO_IDX` INT NOT NULL,
+                              `IDX` INT NOT NULL AUTO_INCREMENT,
+                              `SHOT_DATE` DATE NULL,
+                              `SHOT_TIME` TIME NULL,
+                              `CONTOURED_IMAGE_PATH` VARCHAR(255) NULL,
+                              `CLUSTER_CENTROID_PIXEL` VARCHAR(255) NULL,
+                              `CLUSTER_CENTROID_GEO` VARCHAR(255) NULL,
+                              `CLUSTER_CENTROID_GEO_LATITUTDE` DECIMAL NULL,
+                              `CLUSTER_CENTROID_GEO_LONGITUDE` DECIMAL NULL,
+                              `CLUSTER_WIDTH` DECIMAL NULL,
+                              `CLUSTER_LENGTH` DECIMAL NULL,
+                              `CONTOUR_BOUNDARY_PIXELS` LONGTEXT NULL,
+                              `CONTOUR_BOUNDARY_GEO` LONGTEXT NULL,
+                              `SARGASSUM_DENSITY` DECIMAL NULL,
+                              `SARGASSUM_PREDICTION_GEO` VARCHAR(255) NULL DEFAULT 'Not Predicted',
+                              PRIMARY KEY (`IDX`),
+                              CONSTRAINT `fk_sarg_info_img_info`
+                                FOREIGN KEY (`IMG_INFO_IDX`)
+                                REFERENCES `SARGASSUM_DETECTION`.`IMG_INFO` (`IDX`)
+                                ON DELETE NO ACTION
+                                ON UPDATE NO ACTION);"""
+    cursor.execute(sql)
+    Connection.commit()
+
+
+def dictcursor():
+    return pymysql.cursors.DictCursor
